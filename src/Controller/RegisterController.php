b5f1e19dfe1bf289e76407df41af6cdae8484bc9
Kilian MINETTO <killian.minetto@gmail.com>
2021-02-11 20:32:01 +0100

    Encrypt password

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegisterController extends AbstractController
 {
@@ -21,9 +22,10 @@ class RegisterController extends AbstractController
     /**
      * @Route("/inscription", name="register")
      * @param Request $request
+     * @param UserPasswordEncoderInterface $encoder
      * @return Response
      */
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
@@ -32,6 +34,9 @@ class RegisterController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
+
+            $password = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
 
             $this->entityManager->persist($user);
             $this->entityManager->flush();
