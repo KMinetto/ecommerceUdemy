4e06504f319daf8ce5570b75af4da7422ea3d248
Kilian MINETTO <killian.minetto@gmail.com>
2021-04-24 00:44:36 +0200

    Purchase tunnel: purchase summary + style

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Classes\Cart;
 use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,7 +14,7 @@ class OrderController extends AbstractController
     /**
      * @Route("/commandes", name="order")
      */
-    public function index(): Response
+    public function index(Cart $cart, Request $request): Response
     {
         if (!$this->getUser()->getAdresses()->getValues()) {
             return $this->redirectToRoute('account_address_add');
@@ -22,8 +24,15 @@ class OrderController extends AbstractController
             'user' => $this->getUser(),
         ]);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd($form->getData());
+        }
+
         return $this->render('order/index.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'cart' => $cart->getFull()
         ]);
     }
 }
