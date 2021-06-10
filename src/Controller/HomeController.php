a920b600e75ad228b05da6bebc42806e1c087dd6
Kilian MINETTO <killian.minetto@gmail.com>
2021-06-10 22:04:03 +0200

    Ajout des slides dans le back-office

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Header;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,9 +24,11 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         $products = $this->entity->getRepository(Product::class)->findByIsBest(1);
+        $headers = $this->entity->getRepository(Header::class)->findAll();
 
         return $this->render('home/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'headers' => $headers
         ]);
     }
 }
